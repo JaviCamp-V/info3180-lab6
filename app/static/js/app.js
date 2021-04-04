5e575c8291b5d4846ecb3782d0061515509aeb3c
JaviCamp-V <54679525+JaviCamp-V@users.noreply.github.com>
2021-04-04 01:43:19 -0500

    prelim

--- app/static/js/app.js
+++ app/static/js/app.js
@@ -1,29 +1,115 @@
-/* Add your Application JavaScript */
-const app = Vue.createApp({
+const Home = {
+  name: 'Home',
+  template: `
+    <div class="home">
+      <img src="/static/images/logo.png" alt="VueJS Logo">
+      <h1>{{ welcome }}</h1>
+    </div>
+  `,
   data() {
     return {
-      welcome: 'Hello World! Welcome to VueJS'
+      welcome: "Hello World! Welcome to VueJS News"
     }
   }
+ };
+
+
+ const NewsList = {
+  name:'NewsList',
+  template:`
+<div class="news">
+<h2>News</h2>
+<div class="form-inline d-flex justify-content-center">
+<div class="form-group mx-sm-3 mb-2">
+    <label class="sr-only" for="search">Search</label>
+    <input type="search" name="search" v-model="searchTerm"
+    id="search" class="form-control mb-2 mr-sm-2" placeholder="Enter search term here" />
+    <button class="btn btn-success mb-2"
+    @click="searchNews">Search</button>
+</div>
+</div>
+<ul class="news__list">
+<li v-for="article in articles"
+class="news__item"> <span class ="artitle">{{ article.title }}</span>
+<img :src=article.urlToImage> 
+{{article.description}}
+</li>
+</ul>
+</div>`,
+created() {
+  let self = this;
+  fetch('https://newsapi.org/v2/top-headlines?country=us',{
+  headers: {
+          'Authorization': 'Bearer '
+  }})
+  .then(function(response) {
+  return response.json();
+  })
+  .then(function(data) {
+  console.log(data);
+  self.articles = data.articles;
+  });
+  },
+  data() {
+    return {
+    articles: [],
+    searchTerm: ''
+    }
+    },
+    methods: {
+      searchNews() {
+      let self = this;
+      fetch('https://newsapi.org/v2/everything?q='+
+      self.searchTerm + '&language=en', {
+      headers: {
+      'Authorization': 'Bearer '
+      }
+      })
+      .then(function(response) {
+      return response.json();
+      })
+      .then(function(data) {
+      console.log(data);
+      self.articles = data.articles;
+      });
+      }
+      }
+};
+
+
+
+const app = Vue.createApp({
+  components: {
+    'home': Home,
+    'news-list': NewsList
+  }
 });
+
+const router = VueRouter.createRouter({
+  history: VueRouter.createWebHistory(),
+  routes: [
+  { path: '/', component: Home },
+  { path: '/news', component: NewsList }
+  ]
+ });
+
 
 app.component('app-header', {
   name: 'AppHeader',
   template: `
       <header>
-          <nav class="navbar navbar-expand-lg navbar-dark bg-primary fixed-top">
+          <nav class="navbar navbar-expand-lg navbar-dark bg-success fixed-top">
             <a class="navbar-brand" href="#">VueJS App</a>
             <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
               <span class="navbar-toggler-icon"></span>
             </button>
-
             <div class="collapse navbar-collapse" id="navbarSupportedContent">
               <ul class="navbar-nav mr-auto">
                 <li class="nav-item active">
-                  <a class="nav-link" href="#">Home <span class="sr-only">(current)</span></a>
+                  <router-link to="/" class="nav-link">Home</router-link>
                 </li>
                 <li class="nav-item">
-                  <a class="nav-link" href="#">News</a>
+                  <router-link to="/news" class="nav-link">News</router-link>
                 </li>
               </ul>
             </div>
@@ -49,6 +135,11 @@ app.component('app-footer', {
           year: (new Date).getFullYear()
       }
   }
-})
+});
 
+
+
+
+
+app.use(router)
 app.mount('#app');
